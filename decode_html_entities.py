9f3e1ecbe224ffd4c7dd6a818f9d620d81f2330d
Antonio Pérez <aperez@skarcha.com>
2013-09-26 10:41:20 +0200

    Adapted to Python 3

--- decode_html_entities.py
+++ decode_html_entities.py
@@ -1,9 +1,9 @@
-import sublime, sublime_plugin, HTMLParser
+import sublime, sublime_plugin, html.parser
 
 class DecodeHtmlEntitiesCommand(sublime_plugin.TextCommand):
 
 	def run(self, edit):
-		h = HTMLParser.HTMLParser()
+		h = html.parser.HTMLParser()
 		for region in self.view.sel():
 			content = h.unescape(self.view.substr(region))
 			self.view.replace(edit, region, content)
